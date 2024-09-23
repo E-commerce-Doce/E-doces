efd79a990a7b642e1eba85e0db2bfbc87beec5e5
Geovana Santos <geovanassouza450@gmail.com>
2024-09-22 21:54:32 -0300

    Colocando o css da imgLogin

--- E-doces/projeto/app/view/include/header.php
+++ E-doces/projeto/app/view/include/header.php
@@ -19,7 +19,7 @@
         @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
-        :root{
+        :root {
             --cor-verde: #9BB899;
             --cor-rosa-claro: #FCCEAA;
             --cor-rosa-escuro: #EA4961;
@@ -34,42 +34,97 @@
 
         .navbar {
             background-color: #9BB899;
-            color:#9BB899;
+            color: #9BB899;
             font-weight: bolder;
 
         }
-        .estilo{
-            font-family:var(--font-principal);
+
+        .imgLogin {
+
+            position: absolute;
+            width: 450px;
+            height: 450px;
+            left: 750px;
+            top: 0px;
+
         }
-        .estilo2{
-            font-family:var(--font-secundaria);
+
+        .input-group {
+            position: relative;
         }
-        .color1{
+
+        .input-group .form-control {
+            padding-right: 40px;
+        }
+
+
+        .input-group-append {
+            position: absolute;
+            right: 10px;
+            top: 50%;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
+
+
+        .btn {
+            width: auto;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
+        .input-group-append {
+            cursor: pointer;
+        }
+
+        #togglePassword {
+            background-color: transparent;
+            border: none;
+        }
+
+        #togglePassword:hover {
+            color: #007BFF;
+        }
+
+        .estilo {
+            font-family: var(--font-principal);
+        }
+
+        .estilo2 {
+            font-family: var(--font-secundaria);
+        }
+
+        .color1 {
             color: var(--cor-verde);
         }
-        .color2{
+
+        .color2 {
             color: var(--cor-rosa-claro);
-        }  
-        .cor3{
+        }
+
+        .cor3 {
             color: var(--cor-rosa-escuro);
         }
-        .navbar-brand{
-            font-family:var(--font-principal);
+
+        .navbar-brand {
+            font-family: var(--font-principal);
             font-size: 30px;
         }
 
-        a{
+        a {
             color: #000;
             font-weight: bolder;
         }
 
-        .btn{
+        .btn {
             background-color: #EA4961;
             color: #FCCEAA;
-            font-family:var(--font-secundaria);
+            font-family: var(--font-secundaria);
         }
 
-        .navbar-item li{
+        .navbar-item li {
             margin-left: 30%;
         }
 
