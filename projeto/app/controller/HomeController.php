2034cbf329c0c9575ab2cc0889f144dc987ef950
marcella <101215340+marcellaedu@users.noreply.github.com>
2025-01-18 20:22:42 -0300

    Ajustes
    Ajustes para mudar de home conforme muda o papel.

--- projeto/app/controller/HomeController.php
+++ projeto/app/controller/HomeController.php
@@ -3,37 +3,53 @@
 require_once(__DIR__ . "/Controller.php");
 require_once(__DIR__ . "/../dao/UsuarioDAO.php");
 
-
-class HomeController extends Controller
-{
+class HomeController extends Controller {
 
     private UsuarioDAO $usuarioDAO;
+    private ?Usuario $usuario;
 
-    public function __construct()
-    {
-        //Testar se o usuário está logado
-        if (! $this->usuarioLogado()) {
+    public function __construct() {
+        $this->usuarioDAO = new UsuarioDAO();
+
+        if (!$this->usuarioLogado()) {
             exit;
         }
 
-        $this->usuarioDAO = new UsuarioDAO();
+        $usuarioId = $_SESSION[SESSAO_USUARIO_ID] ?? null;
+        $this->usuario = $usuarioId ? $this->usuarioDAO->findById($usuarioId) : null;
 
-        $this->handleAction();
+        if (!$this->usuario) {
+            echo "Usuário não encontrado!";
+            exit;
+        }
+
+        $this->handleAction();       
     }
 
-    protected function home()
-    {
-
+    protected function home() {
         $totalUsuario = $this->usuarioDAO->count();
         $listaUsuario = $this->usuarioDAO->list();
-       
+
         $dados["totalUsuarios"] = $totalUsuario;
         $dados["listaUsuarios"] = $listaUsuario;
-        $this->loadView("home/home.php", []);
+
+        $papel = $this->usuario->getPapel();
+
+        switch ($papel) {
+            case UsuarioPapel::ADMINISTRADOR:
+                $this->loadView("home/homeAdm.php", $dados);
+                break;
+            case UsuarioPapel::CONFEITEIRO:
+                $this->loadView("home/homeConfeiteiro.php", $dados);
+                break;
+            case UsuarioPapel::CLIENTE:
+                $this->loadView("home/homeCliente.php", $dados);
+                break;
+            default:
+                echo "Papel de usuário não reconhecido!";
+                exit;
+        }
     }
 }
 
-
-
-//Criar o objeto da classe HomeController
 new HomeController();
