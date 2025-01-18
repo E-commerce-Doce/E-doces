125ea7fde6b58a8557fb6e419e8209fcaec1431c
marcella <101215340+marcellaedu@users.noreply.github.com>
2025-01-18 20:24:33 -0300

    Ajuste da home

--- projeto/app/view/home/homeConfeiteiro.php
+++ projeto/app/view/home/homeConfeiteiro.php
@@ -4,13 +4,13 @@
 require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
 ?>
-<link rel="stylesheet" href="<?= BASEURL ?>/view/home/confeiteiro.css">
+<link rel="stylesheet" href="<?= BASEURL ?>/view/home/homeConfeiteiro.css">
 
 
 <div class="conf">
 
     <div class="texto">
-        <h1>Bem vindo confeiteiro!</h1>
+        <h1 class="mt-5">Bem vindo confeiteiro!</h1>
         <h2>Aqui, seu talento transforma sonhos em realidade. <br> Compartilhe suas criações! </h2>
         <a class="btn" href="<?= BASEURL . '/controller/DoceController.php?action=list' ?>"> Cadastrar doces</a>
     </div>
@@ -33,6 +33,23 @@ require_once(__DIR__ . "/../include/menu.php");
             <img src="https://i.pinimg.com/736x/4f/8f/7d/4f8f7d6a15f43211a417d60226871c3f.jpg">
         </div>
 
+    </div>
+</div>
+
+<div class="lembrete">
+    <div class="row suporte">
+        <div class="col-lg-6 info">
+            <h1><strong>Suporte</strong></h1>
+            <h3><strong>Suporte via e-mail</strong></h3>
+            <p>Estamos aqui para facilitar sua jornada!</p>
+            <p><strong>Suporte apenas pelo e-mail</strong></p>
+            <p>Resolva todas as suas dúvidas e comece a <br> vender suas delícias de forma simples e prática!</p>
+            <p><strong>Entre em contato conosco e simplifique <br> suas vendas online agora mesmo!"</strong></p>
+        </div>
+
+        <div class="col-lg-5">
+            
+        </div>
     </div>
 </div>
 
