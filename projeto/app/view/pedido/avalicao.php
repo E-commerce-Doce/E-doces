d70dc4b00a60580e5eb1ef7c0c5688ce15a7224e
Fernanda Neres <101215657+ferneres@users.noreply.github.com>
2025-01-21 20:55:55 -0300

    Correções - GitHub Desktop

--- projeto/app/view/pedido/avalicao.php
+++
@@ -1,8 +0,0 @@
-<?php require_once(__DIR__ . '/../include/header.php'); ?>
-<?php require_once(__DIR__ . '/../include/menu.php'); ?>
-
-
-<?php
-require_once(__DIR__ . "/../include/footer2.php");
-?>
-
