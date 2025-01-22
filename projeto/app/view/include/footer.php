41276bea1c6ea2298174fe0def60d5574e85687c
marcella <101215340+marcellaedu@users.noreply.github.com>
2025-01-22 00:30:53 -0300

    Ajustes
    Responsividade

--- projeto/app/view/include/footer.php
+++ projeto/app/view/include/footer.php
@@ -15,11 +15,12 @@
     <!-- Barra Inferior -->
     <div style="padding: 5px 25px;">
         <div class="container d-flex justify-content-between align-items-center" style="padding: 0 15px; max-width: 100%;">
-            <div style="color: whitesmoke; font-size: 16px; font-weight: 300; display: flex; gap: 15px;">
+            <div class="d-none d-md-flex" style="color: whitesmoke; font-size: 16px; font-weight: 300; display: flex; gap: 15px;">
                 <a href="#pedido" style="color: whitesmoke; text-decoration: none;">Faça um pedido</a>
                 <a href="#sobre" style="color: whitesmoke; text-decoration: none;">Sobre</a>
                 <a href="#confeiteiro" style="color: whitesmoke; text-decoration: none;">Torne-se confeiteiro</a>
             </div>
+            <!-- Copyright -->
             <div style="color: whitesmoke; font-size: 16px; font-weight: 300;">
                 © 2024 Copyright:
                 <a href="https://foz.ifpr.edu.br" target="_blank" style="color: whitesmoke; text-decoration: none;">IFPR (Campus Foz do Iguaçu)</a>
@@ -28,22 +29,11 @@
     </div>
 </footer>
 
-
-
-
 <!-- Bootstrap Icons -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-
-
-</footer>
 
 <!-- BOOTSTRAP: scripts requeridos pelo framework -->
 <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-<!-- Fecha as tags BODY e HTML -->
-</body>
-
-</html>
