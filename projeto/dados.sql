fbbbb22e4be98bf54e3cb7c781208c0e67611bbe
Fernanda Neres <101215657+ferneres@users.noreply.github.com>
2025-01-24 15:14:28 -0300

    Correções GitHub-Desktop

--- projeto/dados.sql
+++
@@ -1,8 +0,0 @@
-
-/*senha root*/
-INSERT INTO Usuario (cpf, papel, nomeCompleto, telefone, login, senha, dataNascimento) VALUES
-('123.456.789-50', 'ADMINISTRADOR', 'ADM', '(45) 99912-9999', 'ameis.contato@gmail.com', '$2y$10$9H8nNzW7tM7cGhy6r59gYuKuflEGKzKGOMPv86yUhJbySUNnnY42y', '2000-11-30');
-
-
-INSERT INTO TipoDoce (descricao) VALUES
-('Torta'),('Bolo'), ('Trufa'),('Cookie'),('Brownie'), ('Brigadeiro');
